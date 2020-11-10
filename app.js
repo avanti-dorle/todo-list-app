28f97fb6b82be6fd4243973c6fa723149b70d496
Avanti Dorle <avanti.dorle10@gmail.com>
2020-11-10 12:16:03 +0530

    Removed unwanted dependencies

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //import all the libraries
 
 const express = require('express');
-const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
@@ -29,14 +28,10 @@ db.connect((err) => {
 
 global.db = db; //global scope, can use db globally, no need to open and close connection everytime 
 
-// configure middleware
-app.set('port', process.env.port || port); // set express to use this port
-app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-app.set('view engine', 'ejs'); // configure template engine
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // parse form data client
-app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
-app.use(fileUpload()); // configure fileupload
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static('./public'));
 
 app.use('/',homeRoute);
 
